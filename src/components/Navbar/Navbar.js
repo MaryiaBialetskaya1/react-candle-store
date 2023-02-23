0f1f457058124fdfe81c33f2cee795174abe4589
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-02-23 15:14:10 -0500

    add info tp Navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+export const Navbar = () => {
+  return (
+    <div>
+      <h1>LumiCandle</h1>
+      <h1>FloralCandleSoul</h1>
+      <h1>LumiBlossomos</h1>
+      <h1>moonbloom</h1>
+      <h1>Candlearia</h1>
+      <h1>SerenityCandle</h1>
+    </div>
+  );
+};
