0d4eb0709226600209e466c11b5808661a73f214
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-02-27 20:01:20 -0500

    add data document

--- src/components/LumiBlossomos/LumiBlossomos.js
+++ src/components/LumiBlossomos/LumiBlossomos.js
@@ -7,6 +7,12 @@ export const LumiBlossomos = () => {
       <div>
         <img className={s.mainImage} src={mainImage} alt="mainimage" />
       </div>
+      <div>
+        <h3 className={s.header}>
+          Welcome spring with new scented candles with over 10 scents to choose
+          from, your home will be ready to welcome the season.
+        </h3>
+      </div>
     </div>
   );
 };
---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const data = [
+  {
+    id: 1,
+    searchNameTerm: "unscented",
+    img: "https://images.unsplash.com/photo-1607713109008-d00372938c2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    price: "$14",
+  },
+  {
+    id: 2,
+    searchNameTerm: "unscented",
+    img: "https://images.unsplash.com/photo-1607713109008-d00372938c2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    price: "$14",
+  },
+  {
+    id: 3,
+    searchNameTerm: "wood",
+    img: "https://images.unsplash.com/photo-1607713109008-d00372938c2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    price: "$14",
+  },
+  {
+    id: 4,
+    searchNameTerm: "wood",
+    img: "https://images.unsplash.com/photo-1607713109008-d00372938c2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    price: "$14",
+  },
+  {
+    id: 5,
+    searchNameTerm: "berries",
+    img: "https://images.unsplash.com/photo-1607713109008-d00372938c2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    price: "$14",
+  },
+  {
+    id: 6,
+    searchNameTerm: "berries",
+    img: "https://images.unsplash.com/photo-1607713109008-d00372938c2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    price: "$14",
+  },
+  {
+    id: 7,
+    searchNameTerm: "unscented",
+    img: "https://images.unsplash.com/photo-1607713109008-d00372938c2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    price: "$14",
+  },
+];
