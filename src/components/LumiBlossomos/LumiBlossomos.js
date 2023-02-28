7251ed47987dc7d432fe9448473910999ab74267
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-02-27 19:28:14 -0500

    start work on new section

--- src/components/LumiBlossomos/LumiBlossomos.js
+++ src/components/LumiBlossomos/LumiBlossomos.js
@@ -1,7 +1,12 @@
+import mainImage from "../LumiBlossomos/images/mainimage.jpg";
+import s from "./LumiBlossomos.module.css";
+
 export const LumiBlossomos = () => {
   return (
     <div>
-      <h1>LumiBlossomos</h1>
+      <div>
+        <img className={s.mainImage} src={mainImage} alt="mainimage" />
+      </div>
     </div>
   );
 };
