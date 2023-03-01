cd96ed1b6c29418c85b928cd327809651b5a5b28
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-02-28 20:10:51 -0500

    add new style

--- src/components/LumiBlossomos/NewArrivals/NewArrivals.js
+++ src/components/LumiBlossomos/NewArrivals/NewArrivals.js
@@ -4,20 +4,18 @@ import image02 from "../images/image02.jpg";
 
 export const NewArrivals = () => {
   return (
-    <div>
-      <div className={s.section}>
-        <h2>There’s plenty more to look forward to!</h2>
+    <div className={s.section}>
+      <h2>There’s plenty more to look forward to!</h2>
+      <div className={s.container}>
         <div>
-          <div>
-            <img className={s.img} src={image01} alt="flower" />
-            <h4>Easter scent is coming!</h4>
-            <h5>We’ve got the right fragrances for the every day.</h5>
-          </div>
-          <div>
-            <img className={s.img} src={image02} alt="flower" />
-            <h4>Be the first to know!</h4>
-            <h5>Members get discount and early access to our new Scents.</h5>
-          </div>
+          <img className={s.img} src={image01} alt="flower" />
+          <h4>Easter scent is coming!</h4>
+          <h5>We’ve got the right fragrances for the every day.</h5>
+        </div>
+        <div>
+          <img className={s.img} src={image02} alt="flower" />
+          <h4>Be the first to know!</h4>
+          <h5>Members get discount and early access to our new Scents.</h5>
         </div>
       </div>
     </div>
