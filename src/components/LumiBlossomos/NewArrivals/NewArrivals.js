f9a3f3d80f6a334f2533f77028f5a1ca95835287
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-03-02 11:06:31 -0500

    add final styles

--- src/components/LumiBlossomos/NewArrivals/NewArrivals.js
+++ src/components/LumiBlossomos/NewArrivals/NewArrivals.js
@@ -1,6 +1,4 @@
 import s from "./NewArrivals.module.css";
-import image01 from "../images/image01.jpg";
-import image02 from "../images/image02.jpg";
 import image00 from "../images/image00.jpg";
 
 export const NewArrivals = ({ newArrivals }) => {
@@ -14,7 +12,17 @@ export const NewArrivals = ({ newArrivals }) => {
       <div>
         <img className={s.mainImage} src={image00} alt="flower" />
       </div>
-      <h2>There’s plenty more to look forward to!</h2>
+      <div className={s.headerContainer}>
+        <h2 className={s.headerFour}>
+          There’s plenty more to look forward to!
+        </h2>
+        <h4 className={s.par}>
+          This is a magical time - the weekend ... this is your way of life,
+          your way of thinking. A little mess in the orderly system of duties
+          and we give you a way to get a boost of energy and improve your mood.
+        </h4>
+      </div>
+
       <div className={s.container}>
         {newArrivals.map((item) => {
           const { id, img, description, par } = item;
@@ -22,8 +30,8 @@ export const NewArrivals = ({ newArrivals }) => {
             <div key={id}>
               <div className={s.imageContainer}>
                 <img className={s.img} src={img} alt="flower" />
-                <h4>{description}</h4>
-                <h5>{par}</h5>
+                <h4 className={s.description}>{description}</h4>
+                <h5 className={s.imgParagraph}>{par}</h5>
               </div>
             </div>
           );
