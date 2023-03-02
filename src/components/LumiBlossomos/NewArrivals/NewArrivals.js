eaabd93a5f1296a93adebfe81864fb5dbd97cee5
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-03-01 21:28:07 -0500

    delete commented code

--- src/components/LumiBlossomos/NewArrivals/NewArrivals.js
+++ src/components/LumiBlossomos/NewArrivals/NewArrivals.js
@@ -24,24 +24,6 @@ export const NewArrivals = ({ newArrivals }) => {
           </div>
         );
       })}
-      {/* <div>
-        <img className={s.mainImage} src={image00} alt="flower" />
-      </div>
-      <div className={s.section}>
-        <h2>There’s plenty more to look forward to!</h2>
-        <div className={s.container}>
-          <div className={s.imageContainer}>
-            <img className={s.img} src={image01} alt="flower" />
-            <h4>Easter scent is coming!</h4>
-            <h5>We’ve got the right fragrances for the every day.</h5>
-          </div>
-          <div className={s.imageContainer}>
-            <img className={s.img} src={image02} alt="flower" />
-            <h4>Be the first to know!</h4>
-            <h5>Members get discount and early access to our new Scents.</h5>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 };
