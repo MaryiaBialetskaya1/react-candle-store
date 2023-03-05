89c9639e7a77d394ee668c106f9d53a70489f4c0
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-03-04 19:36:56 -0500

    edit layout of home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,20 @@
 import { useState } from "react";
 import { CarouselData } from "../../data";
+import s from "./Home.module.css";
 
 export const Home = () => {
-  const [images, setImages] = useState(0);
+  const [images, setImages] = useState(2);
   const { id, img } = CarouselData[images];
 
   return (
     <div key={id}>
-      <button>Prev</button>
-      <img src={img} alt="candles" width="300px" />
-      <button>Next</button>
+      <div className={s.carousel}>
+        <div>
+          <img src={img} className={s.item} alt="candles" />
+        </div>
+        <button className={s.prev}>Prev</button>
+        <button className={s.next}>Next</button>
+      </div>
     </div>
   );
 };
