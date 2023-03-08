955d9627ee3e397200b026cc156265645752ecb1
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-03-08 17:15:13 -0500

    style cards

--- src/components/Home/Programs/Programs.js
+++ src/components/Home/Programs/Programs.js
@@ -2,25 +2,28 @@ import s from "./Programs.module.css";
 
 export const Programs = ({ program }) => {
   return (
-    <div className={s.container}>
-      {program.map((item) => {
-        const { id, img, heading, description } = item;
-        return (
-          <div>
-            <div key={id}>
-              <div>
-                <img src={img} className={s.img} alt="program" />
-              </div>
-              <div>
-                <h3>{heading}</h3>
-              </div>
-              <div>
-                <p>{description}</p>
+    <div>
+      <div>
+        <h3>Candlearia Store Programs</h3>
+      </div>
+      <div className={s.container}>
+        {program.map((item) => {
+          const { id, img, heading, description } = item;
+          return (
+            <div className={s.article}>
+              <div className={s.wrap}>
+                <div className={s.figure}>
+                  <img src={img} className={s.img} alt="program" />
+                </div>
+                <div className={s.articleBody}>
+                  <h3 className={s.header}>{heading}</h3>
+                  <p className={s.paragraph}>{description}</p>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 };
