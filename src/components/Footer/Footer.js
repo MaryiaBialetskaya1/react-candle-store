bda2fcfe310832db2e5632409f1d79701293e18c
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-03-09 21:06:17 -0500

    edit layout

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ export const Footer = () => {
         <div className={s.footerAddr}>
           <h1 className={s.footerLogo}>CandleariaStore</h1>
           <h2>Contact</h2>
-          <address>
+          <address className={s.address}>
             5534 Somewhere In. The World 22193-10212
             <a className={s.footerBtn} href="mailto:example@gmail.com">
               Email Us
@@ -19,34 +19,40 @@ export const Footer = () => {
             <h2 className={s.navTitle}>Media</h2>
             <ul className={s.navUl}>
               <li>
-                <a href="#">Online</a>
+                <a className={s.navUlItem} href="#">
+                  Online
+                </a>
               </li>
               <li>
-                <a href="#">Print</a>
+                <a className={s.navUlItem} href="#">
+                  Print
+                </a>
               </li>
               <li>
-                <a href="#">Alternative Ads</a>
+                <a className={s.navUlItem} href="#">
+                  Alternative Ads
+                </a>
               </li>
             </ul>
           </li>
 
           <li className={s.navItem}>
             <h2 className={s.navTitle}>Technology</h2>
-            <ul className={`${s.navUl} ${s.navUlExtra}`}>
+            <ul className={s.navUl}>
               <li>
-                <a href="#">Hardware Design</a>
+                <a className={s.navUlItem} href="#">
+                  Hardware Design
+                </a>
               </li>
               <li>
-                <a href="#">Software Design</a>
+                <a className={s.navUlItem} href="#">
+                  Software Design
+                </a>
               </li>
               <li>
-                <a href="#">Digital Signage</a>
-              </li>
-              <li>
-                <a href="#">Automation</a>
-              </li>
-              <li>
-                <a href="#">Artificial Intelligence</a>
+                <a className={s.navUlItem} href="#">
+                  Digital Signage
+                </a>
               </li>
             </ul>
           </li>
@@ -55,13 +61,19 @@ export const Footer = () => {
             <h2 className={s.navTitle}>Legal</h2>
             <ul className={s.navUl}>
               <li>
-                <a href="#">Privacy Policy</a>
+                <a className={s.navUlItem} href="#">
+                  Privacy Policy
+                </a>
               </li>
               <li>
-                <a href="#">Terms of Use</a>
+                <a className={s.navUlItem} href="#">
+                  Terms of Use
+                </a>
               </li>
               <li>
-                <a href="#">Sitemap</a>
+                <a className={s.navUlItem} href="#">
+                  Sitemap
+                </a>
               </li>
             </ul>
           </li>
