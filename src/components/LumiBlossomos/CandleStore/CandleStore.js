54a4137d3884e72ef4b65141605eeb04908bd7a2
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-03-15 19:35:11 -0400

    add media query to CandleStore component

--- src/components/LumiBlossomos/CandleStore/CandleStore.js
+++ src/components/LumiBlossomos/CandleStore/CandleStore.js
@@ -8,7 +8,7 @@ export const CandlesStore = ({ candlesStore }) => {
         return (
           <div className={s.container} key={id}>
             <div className={s.wrapper}>
-              <img src={img} alt="candles" width="400px" height="500px" />
+              <img className={s.img} src={img} alt="candles" />
               <h1 className={s.header}> {price}</h1>
               <p className={s.paragraph}>{description}</p>
             </div>
