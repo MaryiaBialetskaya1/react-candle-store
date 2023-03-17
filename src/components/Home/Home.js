f125d0ad0e8a95b9ddeaee9cce1333b7d340bb82
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-03-17 19:36:30 -0400

    add map method to component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,15 +4,17 @@ import s from "./Home.module.css";
 import { Programs } from "./Programs/Programs";
 import { programs } from "../../data";
 import { StoreDescription } from "./StoreDescription/StoreDescription";
+import { storeAvailability } from "../../data";
 
 export const Home = () => {
   const [images, setImages] = useState(0);
   const [program] = useState(programs);
+  const [availability] = useState(storeAvailability);
 
   return (
     <div>
       <HomePageCarousel images={images} setImages={setImages} />
-      <StoreDescription />
+      <StoreDescription availability={availability} />
       <Programs program={program} />
     </div>
   );
--- src/components/Home/StoreDescription/StoreDescription.js
+++ src/components/Home/StoreDescription/StoreDescription.js
@@ -1,7 +1,23 @@
-export const StoreDescription = () => {
+export const StoreDescription = ({ storeAvailability }) => {
   return (
     <div>
       <h1>Store Description</h1>
+      {storeAvailability.map((item) => {
+        const { id, img, name, description } = item;
+        return (
+          <div key={id}>
+            <div>
+              <img src={img} alt="Availability" />
+            </div>
+            <div>
+              <h3>{name}</h3>
+            </div>
+            <div>
+              <h4>{description}</h4>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
