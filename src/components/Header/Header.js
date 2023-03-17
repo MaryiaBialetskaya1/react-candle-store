e8a52a4782b590c42c16bf783deee1f363704e0d
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-03-17 13:22:47 -0400

    update layout style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,29 +5,31 @@ import bag from "../Header/images/bag.png";
 export const Header = () => {
   return (
     <div className={s.container}>
-      <div className={s.flexItem}>
-        <h1 className={s.pageHeader}>Candlearia Store</h1>
-      </div>
-      <div className={s.flexItem}>
-        <input className={s.formInput} type="text" placeholder="Search" />
-      </div>
-      <div className={s.flexItem}>
-        <div className={s.box}>
-          <div className={s.flexItem}>
-            <img className={s.png} src={account} alt="account icon" />
-          </div>
-          <div className={s.flexItem}>
-            <p className={s.paragraph}>account</p>
+      <div className={s.page}>
+        <div className={s.flexItem}>
+          <h1 className={s.pageHeader}>Candlearia Store</h1>
+        </div>
+        <div className={s.flexItem}>
+          <input className={s.formInput} type="text" placeholder="Search" />
+        </div>
+        <div className={s.flexItem}>
+          <div className={s.box}>
+            <div className={s.flexItem}>
+              <img className={s.png} src={account} alt="account icon" />
+            </div>
+            <div className={s.flexItem}>
+              <p className={s.paragraph}>account</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className={s.flexItem}>
-        <div className={s.box}>
-          <div className={s.flexItem}>
-            <img className={s.png} src={bag} alt="shopping bag" />
-          </div>
-          <div className={s.flexItem}>
-            <p className={s.paragraph}>bag</p>
+        <div className={s.flexItem}>
+          <div className={s.box}>
+            <div className={s.flexItem}>
+              <img className={s.png} src={bag} alt="shopping bag" />
+            </div>
+            <div className={s.flexItem}>
+              <p className={s.paragraph}>bag</p>
+            </div>
           </div>
         </div>
       </div>
